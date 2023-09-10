82395e11623afa7873dbc038e9808e73a5fd9165
Adriel Klem <adrielklem16@gmail.com>
2023-09-10 19:36:45 -0300

    update colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,14 +1,16 @@
 export default {
   COLORS: {
-    BACKGROUND_900: "#232129",
-    BACKGROUND_800: "#312E38",
-    BACKGROUND_700: "#3E3B47",
+    BACKGROUND_900: "#0D0C0F",
+    BACKGROUND_800: "#1C1B1E",
+    BACKGROUND_700: "#262529",
+    BACKGROUND_600: "#312E38",
 
     WHITE: "#F4EDE8",
     ORANGE: "#FF9000",
+    PINK: "##FF859B",
 
-    GRAY_100: "#999591",
-    GRAY_300: "#666360",
+    GRAY_100: "#CAC4CF",
+    GRAY_300: "#948F99",
 
     RED: "#FF002E"
   }

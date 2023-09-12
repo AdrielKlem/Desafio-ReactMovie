e483704165c7fab79f81d45c771669d56ca5b1e4
Adriel Klem <adrielklem16@gmail.com>
2023-09-12 20:51:35 -0300

    create components Star

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 
-import { Home } from '../pages/Home'
+import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
     return(
         <BrowserRouter>
-        <Home />
+            <AppRoutes />
         </BrowserRouter>
     )
 }
